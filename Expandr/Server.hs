a429cb0e57074f52076b964a90c148c8cecedef3
Lucas Stadler <mi.au@papill0n.org>
2013-06-29 20:21:53 +0200

    Cache unshortened urls in memory.
    Missing:
    * don't cache non-shortened links, we detect them anyway and don't do
      any network requests for them anyway
    * writing/loading to disk
    * performance tests

--- Expandr/Server.hs
+++ Expandr/Server.hs
@@ -3,14 +3,22 @@ module Expandr.Server where
 
 import Web.Scotty
 import Network.Wai
+import Control.Monad (when)
 import Control.Monad.IO.Class (liftIO)
+import Data.IORef
+import qualified Data.Map as M
 import qualified Data.Text.Lazy as T
 
-import Expandr.Core (getUnshortened)
+import Expandr.Cache (getCachedUnshortened)
 
 server :: IO Application
 server = scottyApp $ do
+    cacheRef <- liftIO $ newIORef M.empty
     get "/" $ do
+        cache <- liftIO $ readIORef cacheRef
         url <- param "url"
-        unshortened <- liftIO $ getUnshortened url 5
+        unshortened <- liftIO $ getCachedUnshortened cache url 5
+        when (not $ M.member url cache) . liftIO $ do
+            putStrLn $ "cache insert: " ++ show (url, unshortened)
+            modifyIORef cacheRef (M.insert url unshortened)
         text $ T.pack unshortened
