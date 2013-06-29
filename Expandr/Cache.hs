8f6226624bda85cb92979235b063a00b26a69614
Lucas Stadler <mi.au@papill0n.org>
2013-06-29 19:41:12 +0200

    Add reading&writing the cache to a json file.

--- Expandr/Cache.hs
+++ Expandr/Cache.hs
@@ -1,6 +1,9 @@
 module Expandr.Cache where
 
 import qualified Data.Map as M
+import qualified Data.ByteString.Lazy as B
+import Data.Aeson
+import System.Directory (doesFileExist)
 
 import Expandr.Core
 
@@ -8,3 +11,11 @@ getCachedUnshortened cache url maxRedirects = do
     case M.lookup url cache of
         Just unshortenedUrl -> return unshortenedUrl
         Nothing -> getUnshortened url maxRedirects
+
+loadCache fileName = do
+    fileExists <- doesFileExist fileName
+    if fileExists
+    then B.readFile fileName >>= return . maybe M.empty id . decode
+    else return M.empty
+
+saveCache fileName cache = B.writeFile fileName $ encode cache
