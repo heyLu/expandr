2f104f3297974e229526980d560f1476d274f959
Lucas Stadler <lu@papill0n.org>
2014-10-04 19:11:02 +0200

    extract expanding into a function.
    so we want use it elsewhere.

--- expandr.js
+++ expandr.js
@@ -15,21 +15,25 @@ function isShortened(url) {
   }
 }
 
-document.addEventListener("mouseover", function(ev) {
-  if (ev.target.tagName == "A" && isShortened(ev.target.href)) {
+function expand(el) {
+  if (el.tagName == "A" && isShortened(el.href)) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://expandr.dev.papill0n.org/?url=' + ev.target.href);
+    xhr.open('GET', 'http://expandr.dev.papill0n.org/?url=' + el.href);
     xhr.setRequestHeader('Accept', 'application/json');
     xhr.onload = function() {
       var expanded = JSON.parse(xhr.response);
       console.log('expandr: ' + expanded.originalUrl + ' -> ' + expanded.expandedUrl);
-      if (ev.target.textContent == ev.target.href) {
-        ev.target.textContent = expanded.expandedUrl;
+      if (el.textContent == el.href) {
+        el.textContent = expanded.expandedUrl;
       }
-      ev.target.href = expanded.expandedUrl;
-      ev.target.title = expanded.expandedUrl;
+      el.href = expanded.expandedUrl;
+      el.title = expanded.expandedUrl;
     }
     xhr.onerror = console.error.bind(console);
     xhr.send();
   }
+}
+
+document.addEventListener("mouseover", function(ev) {
+  expand(ev.target);
 });
