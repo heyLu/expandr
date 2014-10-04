1560898589ab2101dc03f35514ed7cbdd373eb5b
Lucas Stadler <lu@papill0n.org>
2014-10-04 19:01:07 +0200

    add user script that references the main script.

---
+++ expandr.user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name expandr
+// @description Expands shortened urls using https://github.com/heyLu/expandr
+// @include *
+// @require https://heyLu.github.io/expandr/expandr.js
+// ==/UserScript==
