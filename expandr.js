88f940c7c02529d588093dca80854268aa4f753a
Lucas Stadler <lu@papill0n.org>
2014-10-04 18:18:16 +0200

    initial commit: simple page with one example.

---
+++ expandr.js
@@ -0,0 +1,31 @@
+function isShortened(url) {
+  try {
+    url = new URL(url)
+  } catch (e) {
+    return false;
+  }
+
+  switch (url.hostname) {
+    case "t.co":
+    case "bit.ly":
+    case "fb.me":
+      return true;
+    default:
+      return false
+  }
+}
+
+document.addEventListener("mouseover", function(ev) {
+  if (ev.target.tagName == "A" && isShortened(ev.target.href)) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://expandr.dev.papill0n.org/?url=' + ev.target.href);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onload = function() {
+      var expanded = JSON.parse(xhr.response);
+      console.log('expandr: ' + expanded.originalUrl + ' -> ' + expanded.expandedUrl);
+      ev.target.href = expanded.expandedUrl;
+    }
+    xhr.onerror = console.error.bind(console);
+    xhr.send();
+  }
+});
